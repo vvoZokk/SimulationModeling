807be73969aec3bf7d19ac76442d374227fa6114
Alexey <alexey.kozov@gmail.com>
2016-04-27 02:24:22 +0300

    add action functions

--- simulationModeling.go
+++ simulationModeling.go
@@ -27,10 +27,21 @@ const ( // List of actions
 	Use
 	Terminate
 )
+const ( // List of limits
+	Station = iota
+	AC
+	BC
+	Timer
+)
 
 type Checks struct {
 	cur, next int
 	check     bool
+}
+
+type Action struct {
+	Type      int
+	Arguments []int
 }
 
 func GenerateUniform(S *sim.Sim, R *rand.Rand, Limits sim.Pair, PointList []int) {
@@ -44,21 +55,56 @@ func GenerateUniform(S *sim.Sim, R *rand.Rand, Limits sim.Pair, PointList []int)
 	}
 }
 
-func TimerCorrectionPhase(S *sim.Sim, CheckTable map[transaction.Points][]int) {
+func UseBlock(S *sim.Sim, Tr *transaction.Transaction, Time float64, NextPoint int) {
+	if err := S.UsePoint(Tr, Time, NextPoint); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func Phase(S *sim.Sim, R *rand.Rand, TimeTable map[int]sim.Pair, CheckTable map[transaction.Points][]int, RoadMap map[Checks][]Action) {
 	cec, err := S.Extraction()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	for _, tr := range cec {
-		check, err := S.Test(CheckTable[transaction.GetPoints(*tr)])
+		points := transaction.GetPoints(*tr)
+		check, err := S.Test(CheckTable[points])
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		//
-
-		fmt.Println(check)
+		actions := RoadMap[Checks{points.Current, points.Next, check}]
+		for _, action := range actions {
+			if action.Type == Wait {
+				// GEBUG PRINT
+				fmt.Println("WAIT ACTION")
+			}
+			if action.Type == Generate {
+				// GEBUG PRINT
+				fmt.Println("GENERATE ACTION")
+				GenerateUniform(S, R, TimeTable[action.Arguments[0]], []int{action.Arguments[1]})
+			}
+			if action.Type == Use {
+				// GEBUG PRINT
+				fmt.Println("USE ACTION")
+				switch {
+				case action.Arguments[0] == 0:
+					UseBlock(S, tr, 0.0, action.Arguments[1])
+				default:
+					if time, err := sim.Uniform(R, TimeTable[action.Arguments[0]]); err != nil {
+						fmt.Println(err)
+						os.Exit(1)
+					} else {
+						UseBlock(S, tr, time, action.Arguments[1])
+					}
+				}
+			}
+			if action.Type == Terminate {
+				S.Terminate()
+			}
+		}
 	}
 }
 
@@ -66,11 +112,11 @@ func main() {
 
 	// Init section
 
-	timings := map[string]sim.Pair{
-		"Station": sim.Pair{35, 55},
-		"AC":      sim.Pair{12, 18},
-		"BC":      sim.Pair{17, 23},
-		"Timer":   sim.Pair{1440, 1440}}
+	timings := map[int]sim.Pair{
+		Station: sim.Pair{35, 55},
+		AC:      sim.Pair{12, 18},
+		BC:      sim.Pair{17, 23},
+		Timer:   sim.Pair{1440, 1440}}
 
 	checks := map[transaction.Points][]int{
 		transaction.Points{Point0, PointA}:   []int{PointAC, PointCw},
@@ -81,28 +127,30 @@ func main() {
 		transaction.Points{PointCw, PointAC}: []int{PointAC},
 	}
 
-	transfers := map[Checks]string{
-		{Point0, PointA, false}:       "wait",
-		{Point0, PointA, true}:        "tc0_AC + use_A + gen_A", // >A****C****B
-		{PointA, PointAC, true /*_*/}: "tc0_C + use_AC",         // A->***C****B
-		{PointAC, PointC, true}:       "tcUnif_BC + use_C",      // A***->C****B
-		{PointAC, PointC, false}:      "tcUnif_Cw + use_C",      // A***->W****B
-		{PointC, PointCw, true /*_*/}: "tc0_BC + use_Cw",        // A****>W<****B
-		{PointCw, PointBC, false}:     "wait",                   // A****>W<****B
-		{PointCw, PointBC, true}:      "tcUnif_B + use_BC",      // A****W->***B
-		{PointC, PointBC, true /*_*/}: "tcUnif_B + use_BC",      // A****C->***B
-		{PointBC, PointB, true /*_*/}: "tc0_ + use_B",           // A****C***->B
+	transfers := map[Checks][]Action{
+		{Point0, PointA, false}:   []Action{Action{Wait, []int{}}},                                                    // >A****C****B
+		{Point0, PointA, true}:    []Action{Action{Use, []int{0, PointAC}}, Action{Generate, []int{Station, PointA}}}, // >A****C****B
+		{PointA, PointAC, true}:   []Action{Action{Use, []int{0, PointC}}},                                            // A->***C****B
+		{PointAC, PointC, true}:   []Action{Action{Use, []int{AC, PointBC}}},                                          // A***->C****B
+		{PointAC, PointC, false}:  []Action{Action{Use, []int{AC, PointCw}}},                                          // A***->W****B
+		{PointC, PointCw, true}:   []Action{Action{Use, []int{0, PointBC}}},                                           // A****>W<****B
+		{PointCw, PointBC, false}: []Action{Action{Wait, []int{}}},                                                    // A****>W<****B
+		{PointCw, PointBC, true}:  []Action{Action{Use, []int{BC, PointB}}},                                           // A****W->***B
+		{PointC, PointBC, true}:   []Action{Action{Use, []int{BC, PointB}}},                                           // A****C->***B
+		{PointBC, PointB, true}:   []Action{Action{Use, []int{0, Point0}}},                                            // A****C***->B
 
-		{Point0, PointB, false}:       "wait",
-		{Point0, PointB, true}:        "tc0_BC + use_B + gen_B", // A****C****B<
-		{PointB, PointBC, true /*_*/}: "tc0_C + use_BC",         // A****C***<-B
-		{PointBC, PointC, true}:       "tcUnif_BC + use_C",      // A****C<-***B
-		{PointBC, PointC, false}:      "tcUnif_Cw + use_C",      // A****W<-***B
-		{PointC, PointCw, true /*_*/}: "tc0_AC + use_Cw",        // A****>W<****B
-		{PointCw, PointAC, false}:     "wait",                   // A****>W<****B
-		{PointCw, PointAC, true}:      "tcUnif_A + use_AC",      // A***<-W****B
-		{PointC, PointAC, true /*_*/}: "tcUnif_A + use_AC",      // A***<-C****B
-		{PointAC, PointA, true /*_*/}: "tc0_ + use_A",           // A<-***C****B
+		{Point0, PointB, false}:   []Action{Action{Wait, []int{}}},                                                    // A****C****B<
+		{Point0, PointB, true}:    []Action{Action{Use, []int{0, PointBC}}, Action{Generate, []int{Station, PointB}}}, // A****C****B<
+		{PointB, PointBC, true}:   []Action{Action{Use, []int{0, PointC}}},                                            // A****C***<-B
+		{PointBC, PointC, true}:   []Action{Action{Use, []int{BC, PointBC}}},                                          // A****C<-***B
+		{PointBC, PointC, false}:  []Action{Action{Use, []int{BC, PointCw}}},                                          // A****W<-***B
+		{PointC, PointCw, true}:   []Action{Action{Use, []int{0, PointAC}}},                                           // A****>W<****B
+		{PointCw, PointAC, false}: []Action{Action{Wait, []int{}}},                                                    // A****>W<****B
+		{PointCw, PointAC, true}:  []Action{Action{Use, []int{AC, PointA}}},                                           // A***<-W****B
+		{PointC, PointAC, true}:   []Action{Action{Use, []int{AC, PointA}}},                                           // A***<-C****B
+		{PointAC, PointA, true}:   []Action{Action{Use, []int{0, Point0}}},                                            // A<-***C****B
+
+		{Point0, ClockPoint, true}: []Action{Action{Terminate, []int{}}}, // Clock
 	}
 
 	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -111,12 +159,10 @@ func main() {
 
 	// Begin simulation
 
-	GenerateUniform(CLSim, rand, timings["Timer"], []int{Terminate})
-	GenerateUniform(CLSim, rand, timings["Station"], []int{PointA, PointB})
+	GenerateUniform(CLSim, rand, timings[Timer], []int{Terminate})
+	GenerateUniform(CLSim, rand, timings[Station], []int{PointA, PointB})
 	fmt.Println(CLSim)
 
-	fmt.Println(transfers[Checks{PointAC, PointA, true}])
-
-	TimerCorrectionPhase(CLSim, checks)
+	Phase(CLSim, rand, timings, checks, transfers)
 
 }
--- src/sim/sim.go
+++ src/sim/sim.go
@@ -112,6 +112,9 @@ func (s *Sim) UsePoint(Tr *transaction.Transaction, NextTime float64, NextPoint 
 	if err := s.fec.Insert(Tr); err != nil {
 		return err
 	}
+	//
+	fmt.Println(s.pointState)
+	//
 	return nil
 }
 
